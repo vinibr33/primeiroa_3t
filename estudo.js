3d2de81db3eaa1d6445a1a06b0e962d85977215d
vinibr33 <108940365+vinibr33@users.noreply.github.com>
2022-10-05 08:18:46 -0300

    Aula do Dia 05/10/22 Vinicius Lopes N°33 1°A

--- estudo.js
+++ estudo.js
@@ -1,17 +1,33 @@
-let tabuada = 33;
+let tabuada = 1;
 let nome = "Vinicius";
 
 function escreva(){
     document.write("Tabuada do "+ tabuada + "<br>")
-    document.write(tabuada+" x 1 = "+(tabuada*1)+"<br>");
-    document.write(tabuada+" x 2 = "+(tabuada*2)+"<br>");
-    document.write(tabuada+" x 3 = "+(tabuada*3)+"<br>");
-    document.write(tabuada+" x 4 = "+(tabuada*4)+"<br>");
-    document.write(tabuada+" x 5 = "+(tabuada*5)+"<br>");
-    document.write(tabuada+" x 6 = "+(tabuada*6)+"<br>");
-    document.write(tabuada+" x 7 = "+(tabuada*7)+"<br>");
-    document.write(tabuada+" x 8 = "+(tabuada*8)+"<br>");
-    document.write(tabuada+" x 9 = "+(tabuada*9)+"<br>");
-    document.write(tabuada+" x 10 = "+(tabuada*10)+"<br><br>");
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br><br>");
     document.write("Feito por " +nome);
+}
+
+function minhaTabuada(){
+    for(let i = 1; i <= 10; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x " + j + " = "+(j*i)+"<br>");
+        }
+        document.write("<br>");
+    }
+}
+
+function quadrado(){
+    for(let i = 1; i <= 20; i++){
+        document.write("O quadrado de " + i + " é " + (i*i)+ "<br>");
+    }
 }
